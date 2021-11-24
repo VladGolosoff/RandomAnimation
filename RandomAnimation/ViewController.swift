553c0242e049f004fece2ee904ae05c55b91f995
VladGolosoff <v.golosoff@gmail.com>
2021-11-24 19:39:58 +0300

    Pods

--- RandomAnimation/ViewController.swift
+++ RandomAnimation/ViewController.swift
@@ -5,15 +5,33 @@
 //  Created by Владислав Голосов on 24.11.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlets
+    @IBOutlet weak var presetLabel: SpringLabel!
+    @IBOutlet weak var curveLabel: SpringLabel!
+    @IBOutlet weak var forceLabel: SpringLabel!
+    @IBOutlet weak var durationLabel: SpringLabel!
+    @IBOutlet weak var delayLabel: SpringLabel!
+    
+    @IBOutlet weak var animationView: SpringView!
+    
+    @IBOutlet weak var runButton: SpringButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        presetLabel.text = "preset: fadeInUp"
+        curveLabel.text = "curve: easeOutCirc"
+        forceLabel.text = ""
     }
 
-
+    
+    //MARK: - IB Actions
+    @IBAction func runAnimation(_ sender: SpringButton) {
+        runButton.setTitle(<#T##title: String?##String?#>, for: .normal)
+    }
+    
 }
 
